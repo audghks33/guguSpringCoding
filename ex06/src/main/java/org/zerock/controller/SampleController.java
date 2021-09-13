ce6643635b5bf727dfece6af78245184fde3f4ab
kkk2 <audghks3072@gmail.com>
2021-09-13 19:57:48 +0900

    37강 어노테이션을 이용하는 스프링 시큐리티 설정

--- ex06/src/main/java/org/zerock/controller/SampleController.java
+++ ex06/src/main/java/org/zerock/controller/SampleController.java
@@ -1,5 +1,7 @@
 package org.zerock.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,21 @@ public class SampleController {
 	public void doAdmin() {
 		
 		log.info("관리자 권한 접속");
+	}
+	
+	
+	///스프링 시큐리티 설정
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')")
+	@GetMapping
+	public void doMember2() {
+		
+		log.info("logined annotation member");
+	}
+	
+	@Secured({"ROLE_ADMIN"})
+	@GetMapping("/annoAdmin")
+	public void doAdmin2() {
+		
+		log.info("admin annotation only");
 	}
 }
